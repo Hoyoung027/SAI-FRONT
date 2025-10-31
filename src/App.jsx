388bb8bf8c3989e9f92cd4dcbef7b2b706d99ea2
임기주 <kijoo1128@yonsei.ac.kr>
2025-10-31 19:39:46 +0900

    feat: 화면 크기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import SignupScreen from "./components/SignupScreen";
 import MainScreen from "./components/MainScreen";
 
 export default function App() {
-  // 🔹 브라우저 창 높이 가져오기
-  const height = window.innerHeight;
-
   return (
       <div className="flex justify-center items-center bg-white">
         <div className="min-w-[350px] max-w-[500px] w-full h-[100vh] bg-white shadow-md overflow-hidden border border-gray-200">
         <Router>
           <Routes>
             {/* 기본 경로로 들어오면 /login으로 자동 이동 */}
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/" element={<Navigate to="/login" replace />} /> 
+            <Route path="/login" element={<LoginScreen />} /> 
             <Route path="/signup" element={<SignupScreen />} />
             <Route path="/main" element={<MainScreen />} />
           </Routes>
